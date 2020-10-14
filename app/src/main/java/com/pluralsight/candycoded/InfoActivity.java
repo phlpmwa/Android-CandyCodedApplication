63a816344901f0219b40a38e720b1e156c328cd4
mwaphinju <phlpnjuguna@gmail.com>
2020-10-14 10:44:07 +0300

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -41,4 +41,12 @@ public class InfoActivity extends AppCompatActivity {
             startActivity(mapIntent);
         }
     }
+
+    public void createPhoneIntent(View view)
+    {
+        Uri uri= Uri.parse("tel:0123456789");
+        Intent phoneIntent= new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(uri);
+        startActivity(phoneIntent);
+    }
 }
